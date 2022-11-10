a7ef7de67edf8e86f431e4be2b1ba680ef824085
Eduardo Braga <chocolate.blindado123@gmail.com>
2022-11-10 18:55:20 -0300

    Fix: corrigi o erro com o deploy

--- src/components/app/index.jsx
+++ src/components/app/index.jsx
@@ -3,7 +3,7 @@ import { ThemeProvider } from "styled-components";
 import { PomodoroContext } from "../../provider/PomodoroSettings";
 import { GlobalStyles } from '../../styles/globalStyles/style';
 
-import settingsImgURL from '../../assets/icons/settings.PNG';
+import settingsImgURL from '../../assets/icons/settings.png';
 
 import { Button } from "../Button/style";
 import { CircleTimer } from "../CircleTimer";
