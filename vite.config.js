a4c9d7b0f8adc8507d721e72dee2d5d1fd54be6d
Eduardo Braga <chocolate.blindado123@gmail.com>
2022-11-10 18:46:53 -0300

    fix: tentando corrigir o erro com deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    extensions: [
+      ".mjs",
+      ".js",
+      ".ts",
+      ".jsx",
+      ".tsx",
+      ".json",
+      ".vue",
+      ".scss",
+    ],
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      json2csv: "json2csv/dist/json2csv.umd.js"
+    },
+  }
 })
